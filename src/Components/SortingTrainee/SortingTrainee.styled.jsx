da70cb4eb4cd23141b40e76c3531a09e4c8fe4d7
Arseniuk Oleksandr <korol1511@gmail.com>
2023-05-19 16:56:36 +0300

    Add new functionals

--- src/Components/SortingTrainee/SortingTrainee.styled.jsx
+++ src/Components/SortingTrainee/SortingTrainee.styled.jsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 const WrapperBTN = styled.div`
   position: sticky;
-  top: 80px;
+  top: 78px;
   height: auto;
   background-color: #ddd2c5;
   text-align: center;
-  width: 100%;
+  width: 35px;
+  height: 100%;
 `;
 
 const WrapperInfoTime = styled.div`
